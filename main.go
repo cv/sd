dfbcf580cdcf669cfdae89e1afc0292b01cef3e2
Carlos Villela <cv@lixo.org>
2018-04-04 22:30:52 -0300

    Initial brain dump

---
+++ main.go
@@ -0,0 +1,191 @@
+package main
+
+import (
+	"github.com/spf13/cobra"
+	"os"
+	"path/filepath"
+	"io/ioutil"
+	"strings"
+	"fmt"
+	"bufio"
+	"regexp"
+	"syscall"
+)
+
+func main() {
+	root := &cobra.Command{
+		Use: "sd",
+	}
+
+	completions := &cobra.Command{
+		Use:   "completions",
+		Short: "Generate completion scripts",
+		Run: func(cmd *cobra.Command, args []string) {
+			root.Usage()
+		},
+	}
+
+	completions.AddCommand(&cobra.Command{
+		Use:   "bash",
+		Short: "Generate completions for bash",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return root.GenBashCompletion(os.Stdout)
+		},
+	})
+
+	completions.AddCommand(&cobra.Command{
+		Use:   "zsh",
+		Short: "Generate completions for zsh",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return root.GenZshCompletion(os.Stdout)
+		},
+	})
+
+	err := loadCommandsInto(root)
+	if err != nil {
+		panic(err)
+	}
+
+	root.AddCommand(completions)
+	root.ExecuteC()
+}
+
+func loadCommandsInto(root *cobra.Command) error {
+	baseDir := filepath.Join(os.Getenv("HOME"), ".sd")
+	parent := root
+
+	err := filepath.Walk(baseDir, func(path string, info os.FileInfo, err error) error {
+		switch {
+
+		case err != nil, path == baseDir:
+			return nil
+
+		case []rune(filepath.Base(path))[0] == []rune(".")[0]:
+			return filepath.SkipDir
+
+		case filepath.Base(path) == "README":
+			cmd, err := commandFromReadme(path)
+			if err != nil {
+				return err
+			}
+			parent.AddCommand(cmd)
+			parent = cmd
+
+		case info.Mode()&0100 != 0 && !info.IsDir():
+			cmd, err := commandFromScript(path)
+			if err != nil {
+				return err
+			}
+			parent.AddCommand(cmd)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+/*
+
+Looks for a line like this:
+
+# name-of-the-file: short description.
+
+ */
+func shortDescriptionFrom(path string) (string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	r := regexp.MustCompile(fmt.Sprintf(`^# %s: (.*)$`, regexp.QuoteMeta(filepath.Base(path))))
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		match := r.FindStringSubmatch(scanner.Text())
+		if len(match) == 2 {
+			return match[1], nil
+		}
+	}
+	return "", nil
+}
+
+/*
+
+Looks for a line like this:
+
+# example: foo bar 1 2 3
+
+ */
+func exampleFrom(path string) (string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	r := regexp.MustCompile(`^# example: (.*)$`)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		match := r.FindStringSubmatch(scanner.Text())
+		if len(match) == 2 {
+			return "  " + match[1], nil
+		}
+	}
+	return "", nil
+}
+
+func commandFromScript(path string) (*cobra.Command, error) {
+	shortDesc, err := shortDescriptionFrom(path)
+	if err != nil {
+		return nil, err
+	}
+
+	example, err := exampleFrom(path)
+	if err != nil {
+		return nil, err
+	}
+
+	cmd := cobra.Command{
+		Use:   filepath.Base(path),
+		Short: shortDesc,
+		Example: example,
+		Run: func(cmd *cobra.Command, args []string) {
+			sh(path, args)
+		},
+	}
+
+	return &cmd, nil
+}
+
+/*
+
+First line of README is the short description, and everything else is the long description.
+
+ */
+func commandFromReadme(path string) (*cobra.Command, error) {
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	cmd := &cobra.Command{
+		Use:   filepath.Base(filepath.Dir(path)),
+		Short: strings.Split(string(file), "\n")[0],
+		Long:  string(file),
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Usage()
+		},
+	}
+
+	return cmd, nil
+}
+
+func sh(cmd string, args []string) error {
+	args = append([]string{"sh", "-c", cmd}, args...)
+	fmt.Printf("EXEC: %s\n", args)
+	return syscall.Exec("/bin/sh", args, os.Environ())
+}

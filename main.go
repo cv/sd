5bf4f7aabd81553c8301a10308ccc07ab68b1c0c
Carlos Villela <cv@lixo.org>
2018-04-05 09:55:18 -0300

    Organize imports

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"github.com/spf13/cobra"
+	"bufio"
+	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
-	"io/ioutil"
-	"strings"
-	"fmt"
-	"bufio"
 	"regexp"
+	"strings"
 	"syscall"
+
+	"github.com/spf13/cobra"
 )
 
 func main() {

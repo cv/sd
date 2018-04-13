8c6a7a4c072da338cf00e9820f29a42eadd7ff62
Carlos Villela <cv@lixo.org>
2018-04-13 15:42:26 -0300

    More test coverage

--- cli/cli_test.go
+++ cli/cli_test.go
@@ -1,7 +1,9 @@
 package cli
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -68,6 +70,7 @@ func TestInitCompletions(t *testing.T) {
 }
 
 func TestInitDebugging(t *testing.T) {
+	logrus.SetOutput(ioutil.Discard)
 	var restore []string
 	copy(restore, os.Args)
 	defer func() {
@@ -77,10 +80,10 @@ func TestInitDebugging(t *testing.T) {
 	sd := &sd{
 		root: &cobra.Command{},
 	}
-	sd.initCompletions()
 
 	t.Run("sets logrus level", func(t *testing.T) {
 		assert.Equal(t, logrus.InfoLevel, logrus.GetLevel())
+		defer logrus.SetLevel(logrus.InfoLevel)
 
 		os.Args = []string{"-d"}
 		sd.initDebugging()
@@ -218,7 +221,9 @@ func TestExecCommand(t *testing.T) {
 			return ""
 		}
 
+		called := false
 		syscallExec = func(argv0 string, argv []string, envv []string) error {
+			called = true
 			assert.Equal(t, "/bin/sh", argv0)
 			assert.Equal(t, []string{"sh", "-c", "some-visual-editor /path/to/foo"}, argv)
 			return nil
@@ -234,6 +239,7 @@ func TestExecCommand(t *testing.T) {
 
 		err := execCommand(cmd, []string{})
 		assert.NoError(t, err)
+		assert.True(t, called)
 	})
 
 	t.Run("edit with EDITOR", func(t *testing.T) {
@@ -256,7 +262,9 @@ func TestExecCommand(t *testing.T) {
 			return ""
 		}
 
+		called := false
 		syscallExec = func(argv0 string, argv []string, envv []string) error {
+			called = true
 			assert.Equal(t, "/bin/sh", argv0)
 			assert.Equal(t, []string{"sh", "-c", "some-editor /path/to/foo"}, argv)
 			return nil
@@ -272,6 +280,7 @@ func TestExecCommand(t *testing.T) {
 
 		err := execCommand(cmd, []string{})
 		assert.NoError(t, err)
+		assert.True(t, called)
 	})
 
 	t.Run("edit with default vim", func(t *testing.T) {
@@ -288,7 +297,9 @@ func TestExecCommand(t *testing.T) {
 			return ""
 		}
 
+		called := false
 		syscallExec = func(argv0 string, argv []string, envv []string) error {
+			called = true
 			assert.Equal(t, "/bin/sh", argv0)
 			assert.Equal(t, []string{"sh", "-c", "$(which vim) /path/to/foo"}, argv)
 			return nil
@@ -304,6 +315,7 @@ func TestExecCommand(t *testing.T) {
 
 		err := execCommand(cmd, []string{})
 		assert.NoError(t, err)
+		assert.True(t, called)
 	})
 
 	t.Run("exec script", func(t *testing.T) {
@@ -319,7 +331,9 @@ func TestExecCommand(t *testing.T) {
 			return ""
 		}
 
+		called := false
 		syscallExec = func(argv0 string, argv []string, envv []string) error {
+			called = true
 			assert.Equal(t, "/path/to/foo", argv0)
 			assert.Equal(t, []string{"/path/to/foo", "bar"}, argv)
 			return nil
@@ -335,6 +349,101 @@ func TestExecCommand(t *testing.T) {
 
 		err := execCommand(cmd, []string{"bar"})
 		assert.NoError(t, err)
-
+		assert.True(t, called)
 	})
+}
+
+func TestInit(t *testing.T) {
+	t.Run("sets the initialized flag", func(t *testing.T) {
+		sd := &sd{root: &cobra.Command{}}
+		sd.init()
+		assert.True(t, sd.initialized)
+	})
+}
+
+func TestNew(t *testing.T) {
+	t.Run("creates valid instane", func(t *testing.T) {
+		sd := New("1.0.0").(*sd)
+		assert.True(t, sd.initialized)
+		assert.Equal(t, "1.0.0", sd.root.Version)
+	})
+}
+
+func TestShowUsage(t *testing.T) {
+	cmd := &cobra.Command{}
+	called := false
+	cmd.SetUsageFunc(func(c *cobra.Command) error {
+		called = true
+		assert.Equal(t, cmd, c)
+		return nil
+	})
+	showUsage(cmd, []string{})
+	assert.True(t, called)
+}
+
+func TestRunCompletionsBash(t *testing.T) {
+	restore := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+	outC := make(chan string)
+
+	var out string
+	go func() {
+		var buf bytes.Buffer
+		io.Copy(&buf, r)
+		outC <- buf.String()
+	}()
+
+	var args []string
+	copy(args, os.Args)
+	defer func() {
+		copy(os.Args, args)
+	}()
+
+	os.Args = []string{"sd", "completions", "bash"}
+
+	err := New("1.0").Run()
+
+	func() {
+		w.Close()
+		os.Stdout = restore
+		out = <-outC
+	}()
+
+	assert.NoError(t, err)
+
+	assert.Contains(t, out, "__sd_debug()")
+}
+
+func TestRunCompletionsZsh(t *testing.T) {
+	restore := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+	outC := make(chan string)
+
+	var out string
+	go func() {
+		var buf bytes.Buffer
+		io.Copy(&buf, r)
+		outC <- buf.String()
+	}()
+
+	var args []string
+	copy(args, os.Args)
+	defer func() {
+		copy(os.Args, args)
+	}()
+
+	os.Args = []string{"sd", "completions", "zsh"}
+
+	err := New("1.0").Run()
+
+	func() {
+		w.Close()
+		os.Stdout = restore
+		out = <-outC
+	}()
+
+	assert.NoError(t, err)
+	assert.Contains(t, out, "#compdef sd")
 }

f45301db4496f6d90290884a4eca12b925505f6c
Carlos Villela <cv@lixo.org>
2018-04-16 14:19:03 -0700

    DRY up tests

--- cli/parsing_test.go
+++ cli/parsing_test.go
@@ -12,149 +12,163 @@ import (
 )
 
 func TestShortDescriptionFrom(t *testing.T) {
-	t.Run("happy path", func(t *testing.T) {
-		f, err := ioutil.TempFile("", "test-short-description-ok")
-		assert.NoError(t, err)
+	var tests = []struct {
+		name        string
+		inputFormat string
+		expected    string
+	}{
+		{
+			"happy path",
+			"#\n# %s: blah\n#\n",
+			"blah",
+		},
+		{
+			"missing",
+			"#\n#\n#\n",
+			"",
+		},
+		{
+			"no input",
+			"",
+			"",
+		},
+	}
 
-		f.WriteString(fmt.Sprintf("#\n# %s: blah\n#\n", filepath.Base(f.Name())))
-		defer func() {
-			f.Close()
-			os.Remove(f.Name())
-		}()
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			f, err := ioutil.TempFile("", test.name)
+			assert.NoError(t, err)
 
-		v, err := shortDescriptionFrom(f.Name())
-		assert.NoError(t, err)
-		assert.Equal(t, "blah", v)
-	})
-	t.Run("missing", func(t *testing.T) {
-		f, err := ioutil.TempFile("", "test-short-description-missing")
-		assert.NoError(t, err)
+			f.WriteString(fmt.Sprintf(test.inputFormat, filepath.Base(f.Name())))
+			defer func() {
+				f.Close()
+				os.Remove(f.Name())
+			}()
 
-		f.WriteString("#\n#\n#\n")
-		defer func() {
-			f.Close()
-			os.Remove(f.Name())
-		}()
-
-		v, err := shortDescriptionFrom(f.Name())
-		assert.NoError(t, err)
-		assert.Equal(t, "", v)
-	})
+			v, err := shortDescriptionFrom(f.Name())
+			assert.NoError(t, err)
+			assert.Equal(t, test.expected, v)
+		})
+	}
 }
 
 func TestExampleFrom(t *testing.T) {
-	t.Run("happy path", func(t *testing.T) {
-		f, err := ioutil.TempFile("", "test-example-ok")
-		assert.NoError(t, err)
+	var tests = []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			"happy path",
+			"#\n# example: blah\n#\n",
+			"  sd blah",
+		},
+		{
+			"missing",
+			"#\n#\n#\n",
+			"",
+		},
+		{
+			"no input",
+			"",
+			"",
+		},
+	}
 
-		f.WriteString("#\n# example: blah\n#\n")
-		defer func() {
-			_ = f.Close()
-			_ = os.Remove(f.Name())
-		}()
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			f, err := ioutil.TempFile("", test.name)
+			assert.NoError(t, err)
 
-		v, err := exampleFrom(f.Name())
-		assert.NoError(t, err)
-		assert.Equal(t, "  sd blah", v)
-	})
-	t.Run("missing", func(t *testing.T) {
-		f, err := ioutil.TempFile("", "test-example-missing")
-		assert.NoError(t, err)
+			f.WriteString(test.input)
+			defer func() {
+				_ = f.Close()
+				_ = os.Remove(f.Name())
+			}()
 
-		f.WriteString("#\n#\n#\n")
-		defer func() {
-			_ = f.Close()
-			_ = os.Remove(f.Name())
-		}()
-
-		v, err := exampleFrom(f.Name())
-		assert.NoError(t, err)
-		assert.Equal(t, "", v)
-	})
+			v, err := exampleFrom(f.Name())
+			assert.NoError(t, err)
+			assert.Equal(t, test.expected, v)
+		})
+	}
 }
 
 func TestArgsFrom(t *testing.T) {
-	t.Run("happy path with 3 args", func(t *testing.T) {
-		f, err := ioutil.TempFile("", "test-args3-ok")
-		assert.NoError(t, err)
+	var tests = []struct {
+		name  string
+		input string
+		check func(t *testing.T, v cobra.PositionalArgs, err error)
+	}{
+		{
+			"happy path with 2 args",
+			"#\n# args: 2\n#\n",
+			func(t *testing.T, v cobra.PositionalArgs, err error) {
+				assert.NoError(t, err)
+				assert.Error(t, v(&cobra.Command{}, []string{}))
+				assert.Error(t, v(&cobra.Command{}, []string{"first"}))
+				assert.NoError(t, v(&cobra.Command{}, []string{"first", "second"}))
+				assert.Error(t, v(&cobra.Command{}, []string{"first", "second", "third"}))
+			},
+		},
+		{
+			"happy path with no args",
+			"#\n# args: 0\n#\n",
+			func(t *testing.T, v cobra.PositionalArgs, err error) {
+				assert.NoError(t, err)
+				assert.NoError(t, v(&cobra.Command{}, []string{}))
+				assert.Error(t, v(&cobra.Command{}, []string{"first"}))
+				assert.Error(t, v(&cobra.Command{}, []string{"first", "second"}))
+			},
+		},
+		{
+			"missing",
+			"",
+			func(t *testing.T, v cobra.PositionalArgs, err error) {
+				assert.NoError(t, err)
+				assert.NoError(t, v(&cobra.Command{}, []string{}))
+				assert.NoError(t, v(&cobra.Command{}, []string{"first", "second"}))
+			},
+		},
+		{
+			"bad value: not numeric",
+			"#\n# args: banana\n#\n",
+			func(t *testing.T, v cobra.PositionalArgs, err error) {
+				assert.NoError(t, err)
+				assert.NoError(t, v(&cobra.Command{}, []string{}))
+				assert.NoError(t, v(&cobra.Command{}, []string{"first", "second"}))
+			},
+		},
+		{
+			"bad value: too large",
+			"#\n# args: 999999999999999999999999999999999999999999999999999999999999999999999999\n#\n",
+			func(t *testing.T, v cobra.PositionalArgs, err error) {
+				assert.Error(t, err)
+			},
+		},
+		{
+			"no input",
+			"",
+			func(t *testing.T, v cobra.PositionalArgs, err error) {
+				assert.NoError(t, err)
+				assert.NoError(t, v(&cobra.Command{}, []string{}))
+				assert.NoError(t, v(&cobra.Command{}, []string{"first", "second"}))
+			},
+		},
+	}
 
-		f.WriteString("#\n# args: 2\n#\n")
-		defer func() {
-			f.Close()
-			os.Remove(f.Name())
-		}()
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			f, err := ioutil.TempFile("", test.name)
+			assert.NoError(t, err)
 
-		v, err := argsFrom(f.Name())
-		assert.NoError(t, err)
-		assert.Error(t, v(&cobra.Command{}, []string{}))
-		assert.Error(t, v(&cobra.Command{}, []string{"first"}))
-		assert.NoError(t, v(&cobra.Command{}, []string{"first", "second"}))
-		assert.Error(t, v(&cobra.Command{}, []string{"first", "second", "third"}))
-	})
+			f.WriteString(test.input)
+			defer func() {
+				f.Close()
+				os.Remove(f.Name())
+			}()
 
-	t.Run("happy path with no args", func(t *testing.T) {
-		f, err := ioutil.TempFile("", "test-args0-ok")
-		assert.NoError(t, err)
-
-		f.WriteString("#\n# args: 0\n#\n")
-		defer func() {
-			f.Close()
-			os.Remove(f.Name())
-		}()
-
-		v, err := argsFrom(f.Name())
-		assert.NoError(t, err)
-		assert.NoError(t, v(&cobra.Command{}, []string{}))
-		assert.Error(t, v(&cobra.Command{}, []string{"first"}))
-		assert.Error(t, v(&cobra.Command{}, []string{"first", "second"}))
-	})
-
-	t.Run("missing", func(t *testing.T) {
-		f, err := ioutil.TempFile("", "test-args-missing")
-		assert.NoError(t, err)
-
-		f.WriteString("#\n#\n#\n")
-		defer func() {
-			f.Close()
-			os.Remove(f.Name())
-		}()
-
-		v, err := argsFrom(f.Name())
-		assert.NoError(t, err)
-		assert.NoError(t, v(&cobra.Command{}, []string{}))
-		assert.NoError(t, v(&cobra.Command{}, []string{"first", "second"}))
-	})
-
-	t.Run("bad value: not numeric", func(t *testing.T) {
-		f, err := ioutil.TempFile("", "test-args-not-numeric")
-		assert.NoError(t, err)
-
-		f.WriteString("#\n# args: banana\n#\n")
-		defer func() {
-			f.Close()
-			os.Remove(f.Name())
-		}()
-
-		v, err := argsFrom(f.Name())
-		assert.NoError(t, err)
-		assert.NoError(t, v(&cobra.Command{}, []string{}))
-		assert.NoError(t, v(&cobra.Command{}, []string{"first", "second"}))
-	})
-
-	t.Run("bad value: too large", func(t *testing.T) {
-		f, err := ioutil.TempFile("", "test-args-too-large")
-		assert.NoError(t, err)
-
-		f.WriteString("#\n# args: 999999999999999999999999999999999999999999999999999999999999999999999999\n#\n")
-		defer func() {
-			f.Close()
-			os.Remove(f.Name())
-		}()
-
-		v, err := argsFrom(f.Name())
-		if !assert.Error(t, err) {
-			assert.NoError(t, v(&cobra.Command{}, []string{}))
-			assert.NoError(t, v(&cobra.Command{}, []string{"first", "second"}))
-		}
-	})
+			v, err := argsFrom(f.Name())
+			test.check(t, v, err)
+		})
+	}
 }
